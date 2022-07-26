19e0abfbe2e4b21d4d25e691551ef1713cb771a1
schizel19 <patrickleinieldomingo@gmail.com>
2022-07-26 16:54:50 +0800

    add modular notes

---
+++ Ch1_Planning/Projects/Modular/Modular/ModularApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularApp.swift
+//  Modular
+//
+//  Created by Patrick Domingo on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ModularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
