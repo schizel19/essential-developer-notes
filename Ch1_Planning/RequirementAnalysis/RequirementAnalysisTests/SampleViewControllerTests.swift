f495c75fef5079d5ec357545c49d8a4cdc67242e
schizel19 <patrickleinieldomingo@gmail.com>
2022-07-26 15:17:22 +0800

    Fix the test

--- Ch1_Planning/RequirementAnalysis/RequirementAnalysisTests/SampleViewControllerTests.swift
+++ Ch1_Planning/RequirementAnalysis/RequirementAnalysisTests/SampleViewControllerTests.swift
@@ -10,26 +10,13 @@ import XCTest
 
 class SampleViewControllerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testOutputTextIsInputTextOnLoad() throws {
         let viewController = SampleViewController()
         viewController.inputText = "Hello"
         viewController.sampleton = MockSampleton()
         
-        let expectation = expectation(description: "output text is input text")
-        viewController.viewDidLoad()
-        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) { [weak expectation] in
-            expectation?.fulfill()
-        }
-        waitForExpectations(timeout: 1, handler: nil)
-                
+        _ = viewController.view
+        
         XCTAssertEqual(viewController.inputText, viewController.outputText)
     }
 }
