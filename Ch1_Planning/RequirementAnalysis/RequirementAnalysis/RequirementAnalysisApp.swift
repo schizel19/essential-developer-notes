b4b63c098a8bf0e535d88e61052b24bfa61f109b
schizel19 <patrickleinieldomingo@gmail.com>
2022-07-26 11:37:59 +0800

    Chapter 1 Lecture 1 setup project

---
+++ Ch1_Planning/RequirementAnalysis/RequirementAnalysis/RequirementAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequirementAnalysisApp.swift
+//  RequirementAnalysis
+//
+//  Created by Patrick Domingo on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct RequirementAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ch1_Planning/RequirementAnalysis/RequirementAnalysis/Singleton/ApiClient.swift
@@ -0,0 +1,8 @@
+//
+//  ApiClient.swift
+//  RequirementAnalysis
+//
+//  Created by Patrick Domingo on 7/26/22.
+//
+
+import Foundation
