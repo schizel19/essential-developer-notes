7e576b89330883a59814071b78e2709a4e4e3f4c
schizel19 <patrickleinieldomingo@gmail.com>
2022-07-27 12:03:05 +0800

    create feed loader protocol

--- Ch1_Planning/Projects/Modular/Modular/FeedLoader/FeedLoader.swift
+++ Ch1_Planning/Projects/Modular/Modular/FeedLoader/FeedLoader.swift
@@ -5,6 +5,11 @@
 //  Created by Patrick Domingo on 7/27/22
 //
 
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
 protocol FeedLoader {
-    
+    func load(completion: @escaping (LoadFeedResult) -> Void)
 }
