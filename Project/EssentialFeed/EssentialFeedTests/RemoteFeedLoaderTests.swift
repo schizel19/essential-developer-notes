60117c3027cdb065a3854f168c0983e2021d030c
schizel19 <patrickleinieldomingo@gmail.com>
2022-07-27 12:52:58 +0800

    request data from url on load

--- Project/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ Project/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,19 +9,33 @@ import XCTest
 
 
 class RemoteFeedLoader {
-    
+    func load() {
+        HTTPClient.shared.requestURL = URL(string: "https://a-url.com")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init() {}
     var requestURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
      
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        // Arrange
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        // Act
+        sut.load()
+        // Assert
+        XCTAssertNotNil(client.requestURL)
     }
 }
