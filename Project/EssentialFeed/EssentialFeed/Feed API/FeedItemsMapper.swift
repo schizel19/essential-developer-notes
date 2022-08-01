82713208e16557d1bcb08643625119055c290450
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-01 14:02:26 +0800

    move FeedItemsMapper to its own file

---
+++ Project/EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -0,0 +1,36 @@
+//
+//  FeedItemsMapper.swift
+//  EssentialFeed
+//  
+//  Created by Patrick Domingo on 8/1/22
+//
+
+import Foundation
+
+internal final class FeedItemsMapper {
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageUrl: image )
+        }
+    }
+    
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+    
+    private static var OK_200: Int { return 200 }
+
+    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+         
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map(\.item)
+    }
+}
--- Project/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ Project/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -42,30 +42,3 @@ public final class RemoteFeedLoader {
         }
     }
 }
-
-private class FeedItemsMapper {
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
-            throw RemoteFeedLoader.Error.invalidData
-        }
-         
-        let root = try JSONDecoder().decode(Root.self, from: data)
-        return root.items.map(\.item)
-    }
-     
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            FeedItem(id: id, description: description, location: location, imageUrl: image )
-        }
-    }
-
-    private struct Root: Decodable {
-        let items: [Item]
-    }
-
-}
