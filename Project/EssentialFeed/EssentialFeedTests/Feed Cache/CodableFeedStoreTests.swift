93af44e04766f23ddaf19e58af45588cd2946483
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-04 10:45:08 +0800

    extract system under test (set) creation in a factory method

--- Project/EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ Project/EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -74,7 +74,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "wait for completion")
         sut.retrieve { result in
             switch result {
@@ -89,7 +89,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let exp = expectation(description: "wait for completion")
         sut.retrieve { firstResult in
             sut.retrieve { secondResult in
@@ -106,7 +106,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let feed = uniqueImageFeed().local
         let exp = expectation(description: "wait for completion")
         let timestamp = Date()
@@ -127,5 +127,11 @@ class CodableFeedStoreTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
+    func makeSUT() -> CodableFeedStore {
+        return CodableFeedStore()
     }
 }
