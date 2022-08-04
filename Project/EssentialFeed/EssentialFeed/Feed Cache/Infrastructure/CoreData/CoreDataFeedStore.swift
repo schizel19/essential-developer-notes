d4ba14e22854518f498b66b5b6f9f99381aaee60
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-04 16:35:32 +0800

    missed completion

--- Project/EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ Project/EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -13,7 +13,7 @@ public final class CoreDataFeedStore: FeedStore {
     public init() {}
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        
+        completion(.empty)
     }
 
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
