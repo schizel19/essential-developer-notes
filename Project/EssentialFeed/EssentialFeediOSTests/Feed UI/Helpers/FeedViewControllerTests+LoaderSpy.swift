0b3e8dae4c9393183fc69df91942b078886e9b10
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-07 18:03:05 +0800

    move assertions to its own helper file

--- Project/EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewControllerTests+LoaderSpy.swift
+++ Project/EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewControllerTests+LoaderSpy.swift
@@ -9,57 +9,59 @@ import Foundation
 import EssentialFeed
 import EssentialFeediOS
 
-class LoaderSpy: FeedLoader, FeedImageDataLoader {
-   
-    // MARK: - FeedLoader
-    private var feedRequests = [(FeedLoader.Result) -> Void]()
-    
-    var loadFeedCallCount: Int {
-        feedRequests.count
-    }
-    
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        feedRequests.append(completion)
-    }
-    
-    func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
-        feedRequests[index](.success(feed))
-    }
-    
-    func completeFeedLoadingWithError(at index: Int = 0) {
-        let error = NSError(domain: "any error", code: 0)
-        feedRequests[index](.failure(error))
-    }
-    
-    // MARK: - FeedImageDataLoader
-    private struct TaskSpy: FeedImageDataLoaderTask {
-        let cancelCallback: () -> Void
-        func cancel() {
-            cancelCallback()
+extension FeedViewControllerTests {
+    class LoaderSpy: FeedLoader, FeedImageDataLoader {
+       
+        // MARK: - FeedLoader
+        private var feedRequests = [(FeedLoader.Result) -> Void]()
+        
+        var loadFeedCallCount: Int {
+            feedRequests.count
         }
-    }
-    
-    private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
-    
-    var loadedImageURLs: [URL] {
-        return imageRequests.map(\.url)
-    }
-    
-    private(set) var cancelledImageUrls = [URL]()
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            feedRequests.append(completion)
+        }
+        
+        func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
+            feedRequests[index](.success(feed))
+        }
+        
+        func completeFeedLoadingWithError(at index: Int = 0) {
+            let error = NSError(domain: "any error", code: 0)
+            feedRequests[index](.failure(error))
+        }
+        
+        // MARK: - FeedImageDataLoader
+        private struct TaskSpy: FeedImageDataLoaderTask {
+            let cancelCallback: () -> Void
+            func cancel() {
+                cancelCallback()
+            }
+        }
+        
+        private var imageRequests = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedImageURLs: [URL] {
+            return imageRequests.map(\.url)
+        }
+        
+        private(set) var cancelledImageUrls = [URL]()
 
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        imageRequests.append((url, completion))
-        return TaskSpy { [weak self] in
-            self?.cancelledImageUrls.append(url)
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            imageRequests.append((url, completion))
+            return TaskSpy { [weak self] in
+                self?.cancelledImageUrls.append(url)
+            }
         }
-    }
-      
-    func completeImageLoading(with imageData: Data = Data(), at index: Int) {
-        imageRequests[index].completion(.success(imageData))
-    }
-    
-    func completeImageLoadingWithError(at index: Int) {
-        let error = NSError(domain: "any error", code: 0)
-        imageRequests[index].completion(.failure(error))
+          
+        func completeImageLoading(with imageData: Data = Data(), at index: Int) {
+            imageRequests[index].completion(.success(imageData))
+        }
+        
+        func completeImageLoadingWithError(at index: Int) {
+            let error = NSError(domain: "any error", code: 0)
+            imageRequests[index].completion(.failure(error))
+        }
     }
 }
