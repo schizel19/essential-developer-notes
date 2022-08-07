70a6191292199c75cdf4db387e180669dfc9086a
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-07 15:23:50 +0800

    Extract `cancelImageDataLoad(from: URL	)` method from `FeedImageDataLoader` protocol into a new `FeedImageDataLoaderTask` protocol that represents a task that can be cancelled. This way, we respect interface segregation principle and `FeedImageDataLoader` implementations are not forced to be stateful.

--- Project/EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ Project/EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -169,16 +169,21 @@ final class FeedViewControllerTests: XCTestCase {
         }
         
         // MARK: - FeedImageDataLoader
+        private struct TaskSpy: FeedImageDataLoaderTask {
+            let cancelCallback: () -> Void
+            func cancel() {
+                cancelCallback()
+            }
+        }
         
         private(set) var loadedImageURLs =  [URL]()
         private(set) var cancelledImageUrls = [URL]()
 
-        func loadImageData(from url: URL) {
+        func loadImageData(from url: URL) -> FeedImageDataLoaderTask {
             loadedImageURLs.append(url)
-        }
-        
-        func cancelImageDataLoad(from url: URL) {
-            cancelledImageUrls.append(url)
+            return TaskSpy { [weak self] in
+                self?.cancelledImageUrls.append(url)
+            }
         }
     }
 }
