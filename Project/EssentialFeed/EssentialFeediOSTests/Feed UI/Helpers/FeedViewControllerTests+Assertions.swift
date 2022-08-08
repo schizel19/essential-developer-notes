65484e1669d8333247207d3c59a0917807d8adc6
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-08 14:05:15 +0800

    renamed `FeedViewControllerTests` to `FeedUIIntegrationTests` since we are testing the composition of multiple UI components in integration

--- Project/EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewControllerTests+Assertions.swift
+++ Project/EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewControllerTests+Assertions.swift
@@ -9,7 +9,7 @@ import XCTest
 import EssentialFeed
 import EssentialFeediOS
 
-extension FeedViewControllerTests {
+extension FeedUIIntegrationTests {
     func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #filePath, line: UInt = #line) {
         guard sut.numberOfRenderedImageViews() == feed.count else {
             return XCTFail("Expected \(feed.count), got \(sut.numberOfRenderedImageViews()) instead.", file: file, line: line)
