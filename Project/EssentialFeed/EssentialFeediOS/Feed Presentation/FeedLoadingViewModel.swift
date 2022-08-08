2ff5928662a5260b7856e664f4f54ac089b174e8
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-08 11:26:13 +0800

    move view models to its own files

---
+++ Project/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//  
+//  Created by Patrick Domingo on 8/8/22
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ Project/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//  
+//  Created by Patrick Domingo on 8/8/22
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
+
