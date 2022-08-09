f1d08c128dd8f4a3ab9b3be6926672385fa12af6
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-09 10:17:40 +0800

    Move `FeedErrorViewModel` to tis own file

---
+++ Project/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//  
+//  Created by Patrick Domingo on 8/9/22
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
