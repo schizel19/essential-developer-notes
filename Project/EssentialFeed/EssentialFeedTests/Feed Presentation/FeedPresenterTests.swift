312177f61d1c61e817048880a301c950a7d7a8e4
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-09 09:51:50 +0800

    `FeedPresenter` does not send messages to view on init

---
+++ Project/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -0,0 +1,32 @@
+//
+//  FeedPresenterTests.swift
+//  EssentialFeedTests
+//  
+//  Created by Patrick Domingo on 8/9/22
+//
+
+import XCTest
+
+final class FeedPresenter {
+    init(view: Any) {
+        
+    }
+}
+
+class FeedPresenterTests: XCTestCase {
+    
+    func test_init_doesNotSendMessagesToView() {
+        let view = ViewSpy()
+        
+        _ = FeedPresenter(view: view)
+        
+        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+    
+    
+    // MARK: - Helpers
+    
+    private class ViewSpy {
+        let messages = [Any]()
+    }
+}
--- Project/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ Project/EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -26,7 +26,8 @@ final class FeedPresenter {
     }
     
     static var title: String {
-        return NSLocalizedString("FEED_VIEW_TITLE",
+        return NSLocalizedString(
+            "FEED_VIEW_TITLE",
             tableName: "Feed",
             bundle: Bundle(for: FeedPresenter.self),
             comment: "Title for the feed view")
