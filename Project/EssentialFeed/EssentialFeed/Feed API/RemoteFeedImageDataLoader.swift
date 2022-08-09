13c67c3fb9aad6dbed7b9f12cb74dee07b6c2b15
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-09 15:15:43 +0800

    RemoteFeedImageDataLoader.loadImageDataFromURL delivers connectivity error on HTTP client error

--- Project/EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ Project/EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -6,39 +6,59 @@
 //
 
 import Foundation
-
-public class RemoteFeedImageDataLoader: FeedImageDataLoader {
-    let client: HTTPClient
+public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
+    private let client: HTTPClient
     
     public init(client: HTTPClient) {
         self.client = client
     }
     
-    private struct HTTPTaskWrapper: FeedImageDataLoaderTask {
-        let wrapped: HTTPClientTask
-
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+        
         func cancel() {
-            wrapped.cancel()
+            preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+        
+        private func preventFurtherCompletions() {
+            completion = nil
         }
     }
     
     @discardableResult
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        return HTTPTaskWrapper(wrapped: client.get(from: url) { [weak self] result in
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
+            
             switch result {
             case let .success((data, response)):
                 if response.statusCode == 200, !data.isEmpty {
-                    completion(.success(data))
+                    task.complete(with: .success(data))
                 } else {
-                    completion(.failure(Error.invalidData))
+                    task.complete(with: .failure(Error.invalidData))
                 }
-            case let .failure(error): completion(.failure(error))
+            
+            case .failure:
+                task.complete(with: .failure(Error.connectivity))
             }
-        })
-    }
-    
-    public enum Error: Swift.Error {
-        case invalidData
+        }
+        return task
     }
 }
