5780e1050ebbb538c30a166ed2f2502e94b6309b
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-10 07:39:30 +0800

    `LocalFeedImageDataLoader.loadImageData` requests stored data for URL from `FeedImageDataStore`

--- Project/EssentialFeed/EssentialFeedTests/Feed API/LocalFeedImageDataLoaderTests.swift
+++ Project/EssentialFeed/EssentialFeedTests/Feed API/LocalFeedImageDataLoaderTests.swift
@@ -6,23 +6,48 @@
 //
 
 import XCTest
+import EssentialFeed
 
-final class LocalFeedImageDataLoader {
-    init(store: Any) {
+protocol FeedImageDataStore {
+    func retrieve(dataForURL url: URL)
+}
 
+final class LocalFeedImageDataLoader: FeedImageDataLoader {
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel() {}
+    }
+
+    private let store: FeedImageDataStore
+
+    init(store: FeedImageDataStore) {
+        self.store = store
+    }
+
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        store.retrieve(dataForURL: url)
+        return Task()
     }
 }
 
 
 final class LocalFeedImageDataLoaderTests: XCTestCase {
 
-    
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
         
         
         XCTAssertTrue(store.receivedMessages.isEmpty)
     }
+    
+    func test_loadImageDataFromURL_requestsStoredDataForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+
+        _ = sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
+    }
+
     
     // MARK: - Helpers
     
@@ -34,7 +59,15 @@ final class LocalFeedImageDataLoaderTests: XCTestCase {
         return (sut, store)
     }
     
-    private class FeedStoreSpy {
-        let receivedMessages = [Any]()
+    private class FeedStoreSpy: FeedImageDataStore {
+        enum Message: Equatable {
+            case retrieve(dataFor: URL)
+        }
+
+        private(set) var receivedMessages = [Message]()
+
+        func retrieve(dataForURL url: URL) {
+            receivedMessages.append(.retrieve(dataFor: url))
+        }
     }
 }
