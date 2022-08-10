e131421bd3f13b052ea25d9e15fcd4f425633a94
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-10 09:28:36 +0800

    LocalFeedLoader.validateCache fails on deletion error of a expired cache

--- Project/EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ Project/EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  ValidateFeedCacheUseCase.swift
+//  ValidateFeedCacheUseCaseTests.swift
 //  EssentialFeedTests
 //  
 //  Created by Patrick Domingo on 8/3/22
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class ValidateFeedCacheUseCase: XCTestCase {
+class ValidateFeedCacheUseCaseTests: XCTestCase {
     
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
@@ -99,6 +99,31 @@ class ValidateFeedCacheUseCase: XCTestCase {
 
         expect(sut, toCompleteWith: .success(()), when: {
             store.completeRetrieval(with: feed.local, timestamp: nonExpiredTimestamp)
+        })
+    }
+
+    func test_validateCache_failsOnDeletionErrorOfExpiredCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let deletionError = anyNSError()
+
+        expect(sut, toCompleteWith: .failure(deletionError), when: {
+            store.completeRetrieval(with: feed.local, timestamp: expiredTimestamp)
+            store.completeDeletion(with: deletionError)
+        })
+    }
+
+    func test_validateCache_succeedsOnSuccessfulDeletionOfExpiredCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let expiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeRetrieval(with: feed.local, timestamp: expiredTimestamp)
+            store.completeDeletionSuccessfully()
         })
     }
 
