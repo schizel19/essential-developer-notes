8278b0d429cc52dcd5ca79c173374eb827ebe115
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-11 16:08:52 +0800

    displays remote feed on launch when customer has connectivity

--- Project/EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ Project/EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -43,6 +43,10 @@ extension FeedViewController {
         return view
     }
     
+    func renderedFeedImageData(at index: Int) -> Data? {
+        return simulateFeedImageViewVisible(at: index)?.renderedImage
+    }
+    
     var isShowingLoadingIndicator: Bool {
         return refreshControl?.isRefreshing == true
     }
