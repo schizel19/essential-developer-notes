6b2d636f32f6fc122ae576fb53180e54c45d07b3
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-12 14:02:38 +0800

    Replace FeedLoader composition with Combine operators

--- Project/EssentialApp/EssentialApp/Composers/FeedUIComposer.swift
+++ Project/EssentialApp/EssentialApp/Composers/FeedUIComposer.swift
@@ -8,13 +8,31 @@
 import EssentialFeed
 import EssentialFeediOS
 import UIKit
+import Combine
 
 public final class FeedUIComposer {
     private init() { }
     
-    public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
+//    public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
+//        let presentationAdapter = FeedLoaderPresentationAdapter(
+//            feedLoader: { })
+//
+//        let feedController = FeedViewController.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
+//
+//        presentationAdapter.presenter = FeedPresenter(
+//            feedView: FeedViewAdapter(
+//                controller: feedController,
+//                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
+//            loadingView: WeakRefVirtualProxy(feedController),
+//            errorView: WeakRefVirtualProxy(feedController)
+//        )
+//
+//        return feedController
+//    }
+    
+    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(
-            feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
+            feedLoader: { feedLoader().dispatchOnMainQueue() })
         
         let feedController = FeedViewController.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
         
