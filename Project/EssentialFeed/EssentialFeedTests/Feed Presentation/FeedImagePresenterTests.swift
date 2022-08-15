8425df01a361d738a0e332f9047c4be5941f7b23
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-15 14:48:53 +0800

    remove unused FeedImagePresenter logic

--- Project/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ Project/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -13,38 +13,9 @@ class FeedImagePresenterTests: XCTestCase {
     func test_map_createsViewModel() {
         let image = uniqueImage()
         
-        let viewModel = FeedImagePresenter<ViewSpy, AnyImage>.map(image)
+        let viewModel = FeedImagePresenter.map(image)
         
         XCTAssertEqual(viewModel.description, image.description)
         XCTAssertEqual(viewModel.location, image.location)
     }
-    
-    // MARK: - Helpers
-    
-    private func makeSUT(
-        imageTransformer: @escaping (Data) -> AnyImage? = { _ in nil },
-        file: StaticString = #file,
-        line: UInt = #line
-    ) -> (sut: FeedImagePresenter<ViewSpy, AnyImage>, view: ViewSpy) {
-        let view = ViewSpy()
-        let sut = FeedImagePresenter(view: view, imageTransformer: imageTransformer)
-        trackForMemoryLeaks(view, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        return (sut, view)
-    }
-    
-    private var fail: (Data) -> AnyImage? {
-        return { _ in nil }
-    }
-    
-    private struct AnyImage: Equatable {}
-    
-    private class ViewSpy: FeedImageView {
-        private(set) var messages = [FeedImageViewModel<AnyImage>]()
-        
-        func display(_ model: FeedImageViewModel<AnyImage>) {
-            messages.append(model)
-        }
-    }
-
 }
