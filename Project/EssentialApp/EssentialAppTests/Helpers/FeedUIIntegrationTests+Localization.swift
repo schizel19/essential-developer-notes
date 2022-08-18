5afb81abc8fd9d5a7eb86acddf6f025fc15d2b88
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-18 09:54:23 +0800

    move localization helpers to shared scope

--- Project/EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Localization.swift
+++ Project/EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -10,19 +10,5 @@ import XCTest
 import EssentialFeed
 
 extension FeedUIIntegrationTests {
-    private class DummyView: ResourceView {
-        func display(_ viewModel: Any) { }
-    }
-    
-    var loadError: String {
-        LoadResourcePresenter<Any, DummyView>.loadError
-    }
-    
-    var feedTitle: String {
-        FeedPresenter.title
-    }
-    
-    var commentsTitle: String {
-        ImageCommentsPresenter.title
-    }
+
 }
--- Project/EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ Project/EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -9,22 +9,34 @@ import Foundation
 import XCTest
 import EssentialFeed
 
-extension XCTestCase {
-    func anyData() -> Data {
-        return Data("any data".utf8)
-    }
-
-    func anyURL() -> URL {
-        return URL(string: "http://a-url.com")!
-    }
-
-    func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
-    }
-    
-    func uniqueFeed() -> [FeedImage] {
-        return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
-    }
-    
+func anyData() -> Data {
+    return Data("any data".utf8)
 }
 
+func anyURL() -> URL {
+    return URL(string: "http://a-url.com")!
+}
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func uniqueFeed() -> [FeedImage] {
+    return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
+}
+
+private class DummyView: ResourceView {
+    func display(_ viewModel: Any) { }
+}
+
+var loadError: String {
+    LoadResourcePresenter<Any, DummyView>.loadError
+}
+
+var feedTitle: String {
+    FeedPresenter.title
+}
+
+var commentsTitle: String {
+    ImageCommentsPresenter.title
+}
