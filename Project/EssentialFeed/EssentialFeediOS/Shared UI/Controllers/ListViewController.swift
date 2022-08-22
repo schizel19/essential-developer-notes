38f628caf5db6706f26d1d03ba101e045d60071e
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-22 13:20:13 +0800

    use iOS 14.0

--- Project/EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ Project/EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -61,7 +61,11 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
             snapshot.appendItems(cellControllers, toSection: section)
         }
         
-        dataSource.applySnapshotUsingReloadData(snapshot)
+        if #available(iOS 15.0, *) {
+            dataSource.applySnapshotUsingReloadData(snapshot)
+        } else {
+            dataSource.apply(snapshot)
+        }
     }
 
     public func display(_ viewModel: ResourceLoadingViewModel) {
