2cb4072e954f83f0848868a98b55f0e1a5130146
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-22 09:12:18 +0800

    Cache page results

--- Project/EssentialApp/EssentialApp/Helpers/Paginated+Combine.swift
+++ Project/EssentialApp/EssentialApp/Helpers/Paginated+Combine.swift
@@ -9,7 +9,6 @@ import Combine
 import EssentialFeed
 
 public extension Paginated {
-    
     init(items: [Item], loadMorePublisher: (() -> AnyPublisher<Self, Error>)?) {
         self.init(items: items, loadMore: loadMorePublisher.map { publisher in
             return { completion in
@@ -26,7 +25,7 @@ public extension Paginated {
     
     var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
         guard let loadMore = loadMore else { return nil }
-
+        
         return {
             Deferred {
                 Future(loadMore)
