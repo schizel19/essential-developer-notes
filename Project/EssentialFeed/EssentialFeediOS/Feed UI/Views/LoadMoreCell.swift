88215beaf1fa617db7fb1845805f30412029b781
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-22 09:28:04 +0800

    Group "Load more" tests

--- Project/EssentialFeed/EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
+++ Project/EssentialFeed/EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
@@ -8,18 +8,18 @@
 import UIKit
 
 public class LoadMoreCell: UITableViewCell {
-
+    
     private lazy var spinner: UIActivityIndicatorView = {
         let spinner = UIActivityIndicatorView(style: .medium)
         contentView.addSubview(spinner)
-
+        
         spinner.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40)
         ])
-
+        
         return spinner
     }()
     
@@ -31,7 +31,7 @@ public class LoadMoreCell: UITableViewCell {
         label.textAlignment = .center
         label.adjustsFontForContentSizeCategory = true
         contentView.addSubview(label)
-
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
@@ -39,10 +39,10 @@ public class LoadMoreCell: UITableViewCell {
             label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
             contentView.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 8),
         ])
-
+        
         return label
     }()
-
+    
     public var isLoading: Bool {
         get { spinner.isAnimating }
         set {
@@ -58,5 +58,5 @@ public class LoadMoreCell: UITableViewCell {
         get { messageLabel.text }
         set { messageLabel.text = newValue }
     }
-
+    
 }
