0a417dd0112ee0d405ae1164b17f3d3ced37fabf
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-22 08:58:23 +0800

    Render load more error message

--- Project/EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ Project/EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -224,6 +224,22 @@ class FeedUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.errorMessage, nil)
     }
     
+    func test_loadMoreCompletion_rendersErrorMessageOnError() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading()
+
+        sut.simulateLoadMoreFeedAction()
+        XCTAssertEqual(sut.loadMoreFeedErrorMessage, nil)
+
+        loader.completeLoadMoreWithError()
+        XCTAssertEqual(sut.loadMoreFeedErrorMessage, loadError)
+
+        sut.simulateLoadMoreFeedAction()
+        XCTAssertEqual(sut.loadMoreFeedErrorMessage, nil)
+    }
+
+    
     // MARK: - Image View Tests
     func test_feedImageView_loadsImageURLWhenVisible() {
         let image0 = makeImage(url: URL(string: "http://url-0.com")!)
