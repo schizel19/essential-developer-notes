c571bdf9fe49c9da28409aba542593a9fb38cd74
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-22 11:15:50 +0800

    Remove unused async methods

--- Project/EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ Project/EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -23,7 +23,7 @@ public final class CoreDataFeedStore {
         guard let model = CoreDataFeedStore.model else {
             throw StoreError.modelNotFound
         }
-
+        
         do {
             container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
             context = container.newBackgroundContext()
@@ -37,13 +37,20 @@ public final class CoreDataFeedStore {
         context.perform { action(context) }
     }
     
+    func performSync<R>(_ action: (NSManagedObjectContext) -> Result<R, Error>) throws -> R {
+        let context = self.context
+        var result: Result<R, Error>!
+        context.performAndWait { result = action(context) }
+        return try result.get()
+    }
+    
     private func cleanUpReferencesToPersistentStores() {
         context.performAndWait {
             let coordinator = self.container.persistentStoreCoordinator
             try? coordinator.persistentStores.forEach(coordinator.remove)
         }
     }
-
+    
     deinit {
         cleanUpReferencesToPersistentStores()
     }
