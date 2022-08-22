a59348b25ad031672463a02240ee51f6054f63ae
schizel19 <patrickleinieldomingo@gmail.com>
2022-08-22 13:09:42 +0800

    user iPhone 13 snapshot

--- Project/EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
+++ Project/EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
@@ -18,20 +18,21 @@ struct SnapshotConfiguration {
     let safeAreaInsets: UIEdgeInsets
     let layoutMargins: UIEdgeInsets
     let traitCollection: UITraitCollection
-
-    static func iPhone8(style: UIUserInterfaceStyle, contentSize: UIContentSizeCategory = .medium) -> SnapshotConfiguration {
+    
+    static func iPhone13(style: UIUserInterfaceStyle, contentSize: UIContentSizeCategory = .medium) -> SnapshotConfiguration {
         return SnapshotConfiguration(
-            size: CGSize(width: 375, height: 667),
-            safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
-            layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
+            size: CGSize(width: 390, height: 844),
+            safeAreaInsets: UIEdgeInsets(top: 47, left: 0, bottom: 34, right: 0),
+            layoutMargins: UIEdgeInsets(top: 55, left: 8, bottom: 42, right: 8),
             traitCollection: UITraitCollection(traitsFrom: [
-                .init(forceTouchCapability: .available),
+                .init(forceTouchCapability: .unavailable),
                 .init(layoutDirection: .leftToRight),
                 .init(preferredContentSizeCategory: contentSize),
                 .init(userInterfaceIdiom: .phone),
                 .init(horizontalSizeClass: .compact),
                 .init(verticalSizeClass: .regular),
-                .init(displayScale: 2),
+                .init(displayScale: 3),
+                .init(accessibilityContrast: .normal),
                 .init(displayGamut: .P3),
                 .init(userInterfaceStyle: style)
             ]))
@@ -39,8 +40,8 @@ struct SnapshotConfiguration {
 }
 
 private final class SnapshotWindow: UIWindow {
-    private var configuration: SnapshotConfiguration = .iPhone8(style: .light)
-
+    private var configuration: SnapshotConfiguration = .iPhone13(style: .light)
+    
     convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
         self.init(frame: CGRect(origin: .zero, size: configuration.size))
         self.configuration = configuration
@@ -49,11 +50,11 @@ private final class SnapshotWindow: UIWindow {
         self.isHidden = false
         root.view.layoutMargins = configuration.layoutMargins
     }
-
+    
     override var safeAreaInsets: UIEdgeInsets {
         return configuration.safeAreaInsets
     }
-
+    
     override var traitCollection: UITraitCollection {
         return UITraitCollection(traitsFrom: [super.traitCollection, configuration.traitCollection])
     }
--- Project/EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ Project/EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -16,8 +16,8 @@ class ListSnapshotTests: XCTestCase {
         
         sut.display(emptyList())
         
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_LIST_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_LIST_dark")
+        record(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "EMPTY_LIST_light")
+        record(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "EMPTY_LIST_dark")
     }
     
     func test_listWithErrorMessage() {
@@ -25,17 +25,17 @@ class ListSnapshotTests: XCTestCase {
         
         sut.display(.error(message: "This is a\nmulti-line\nerror message"))
         
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light, contentSize: .extraExtraExtraLarge)), named: "LIST_WITH_ERROR_MESSAGE_light_extraExtraExtraLarge")
+        record(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "LIST_WITH_ERROR_MESSAGE_light")
+        record(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "LIST_WITH_ERROR_MESSAGE_dark")
+        record(snapshot: sut.snapshot(for: .iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "LIST_WITH_ERROR_MESSAGE_light_extraExtraExtraLarge")
     }
     
     // MARK: - Helpers
     
     private func makeSUT() -> ListViewController {
         let controller = ListViewController()
-        controller.tableView.separatorStyle = .none
         controller.loadViewIfNeeded()
+        controller.tableView.separatorStyle = .none
         controller.tableView.showsVerticalScrollIndicator = false
         controller.tableView.showsHorizontalScrollIndicator = false
         return controller
